dd95b85142643fa6d061c839d014ce7e23a8abdc
Magdalena Jaworska <jaworskam0107@gmail.com>
2019-04-08 12:35:04 +0200

    recipe_project

---
+++ src/js/config.js
@@ -0,0 +1,13 @@
+export const proxy = "https://cors-anywhere.herokuapp.com/";
+export const key =  "ea4105eff584d005896c4da20d57b80d";
+
+//mój "8c65a27b3b8cc2e5c852afa984c786df";
+
+//"ea4105eff584d005896c4da20d57b80d";
+
+//"32c057851bd655e94de2c1673d05589b"; 
+
+//"4fba4c8dbee38e78389e2a35ec535c5d"; 
+
+
+//"https://cors-anywhere.herokuapp.com/";
